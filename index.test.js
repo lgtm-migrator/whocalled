e29cd120ab4bfe77a60917a75bd8340b8bc00d7c
Joseph W Becher <jwbecher@drazisil.com>
2018-01-23 11:24:55 -0500

    When you move the test, change the test

--- index.test.js
+++ index.test.js
@@ -15,10 +15,10 @@
 
 const whoCalled = require('./index');
 
-test('should return index.test.js, line 4', () => {
+test('should return index.test.js, line 19', () => {
   const whoCalledResult = whoCalled();
   expect(whoCalledResult).toContain('index.test.js');
-  expect(whoCalledResult).toContain(':4:');
+  expect(whoCalledResult).toContain(':19:');
 });
 
 console.log(whoCalled(), 'Testing');
